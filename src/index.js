5e107b68ef8564170cfe482abfc158253027950a
Nobuo Masukawa <43978362+mastin0413@users.noreply.github.com>
2021-06-07 13:03:42 +0900

    5-33

--- src/index.js
+++ src/index.js
@@ -7,14 +7,32 @@ const onClickAdd = () => {
 
   //divを生成
   const div = document.createElement("div");
-  div.className = "list-low";
+  div.className = "list-row";
 
-  //li tag
+  //li生成
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button 完了タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button 削除タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   //divタグの子要素に各要素を足す
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  console.log(div);
 
   //未完了リストについて
   document.getElementById("incomplete-list").appendChild(div);
