fcd982a583d7917a8375e26b1e61e968c006878b
Nobuo Masukawa <43978362+mastin0413@users.noreply.github.com>
2021-06-07 12:34:32 +0900

    5-32

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得して初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divを生成
+  const div = document.createElement("div");
+  div.className = "list-low";
+
+  //li tag
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を足す
+  div.appendChild(li);
+
+  //未完了リストについて
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
